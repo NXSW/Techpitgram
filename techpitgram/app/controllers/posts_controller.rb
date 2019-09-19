cb2dbb4c30ab7a3f36e2c53542ae5074af104b6e
49497974m <49497974tkhsm>
2019-09-19 16:22:53 +0900

    Set controller for image post

--- techpitgram/app/controllers/posts_controller.rb
+++ techpitgram/app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(caption: params[:caption], image: params[:image])
+  end
+
+  def delete
+    
   end
 end
