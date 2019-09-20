a63c110b4006f9fd78b50e99c90ccc5f7f183faf
49497974m <49497974tkhsm>
2019-09-20 18:27:18 +0900

    Change resource → resources

--- techpitgram/config/routes.rb
+++ techpitgram/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resource :posts, only: [:index, :new, :create, :destroy]
+  resources :posts, only: [:index, :new, :create, :destroy]
   root 'posts#index'
 
 end
