206b16f4b06f4c208c950eeae708987066135a09
49497974m <49497974tkhsm>
2019-09-24 13:27:16 +0900

    Write create action
    Set before_action and strong parameter

--- techpitgram/app/controllers/comments_controller.rb
+++ techpitgram/app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+before_action :set_post
+
+  def create
+    @comment = @post.comments.new(commnt_params)
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.json
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
 end
