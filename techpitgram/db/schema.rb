77accd17efae2b2ce1cc4ffeab676581d40c841b
49497974m <49497974tkhsm>
2019-09-24 16:55:09 +0900

    Delete user_id column of comment model

--- techpitgram/db/schema.rb
+++ techpitgram/db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_024219) do
+ActiveRecord::Schema.define(version: 2019_09_24_074650) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
-    t.bigint "user_id"
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -45,6 +43,5 @@ ActiveRecord::Schema.define(version: 2019_09_24_024219) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
