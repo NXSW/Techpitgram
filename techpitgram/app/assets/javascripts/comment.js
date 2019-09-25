d19099df4a0efe8bcf2e67f1e0755165f8111a5a
49497974m <49497974tkhsm>
2019-09-25 18:21:06 +0900

    Write javascript to catch the response

--- techpitgram/app/assets/javascripts/comment.js
+++ techpitgram/app/assets/javascripts/comment.js
@@ -1,31 +1,6 @@
-function buildHTML(comment) {
-  var html = `<%= ${ comment.name } %>
-              <%= ${ comment.text } %>
-              <%= link_to '削除', "/posts/${comment.post_id}/comments/${comment.id}", method: :delete %>`
-  return html;
-}
-
-$('.new_comments').on('submit', function(e) {
-  console.log("1")
-  e.preventDefault();
-  console.log("2")
-  var formData = new FormData(this);
-  var href = window.location.href + '/comments'
-  console.log(href);
-  $.ajax({
-    url: href,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-  .done(function (data) {
-    var html = buildHTML(data);
-    $('.comments').prepend(html);
-    $('.comment_text').val('')
-  })
-  .fail(function () {
-    alert("エラー")
-  })
-})
+$(function() {
+  $('.comment').on( "click", ".comment_remove", function () {
+    console.log(this)
+    $(this).parent().remove();
+  });
+});
