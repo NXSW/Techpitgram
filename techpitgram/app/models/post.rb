4ce544bd4dd266fb76822c075b93f21f6a108504
49497974m <49497974tkhsm>
2019-09-26 17:51:48 +0900

    Set association between like model, post model and user model

--- techpitgram/app/models/post.rb
+++ techpitgram/app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :like_users, through: :likes, source: :user
 end
