8d33243d43431a001bd864afc994e4eb91a48428
49497974m <49497974tkhsm>
2019-09-27 16:12:17 +0900

    Added prefix specification to routing

--- techpitgram/config/routes.rb
+++ techpitgram/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :comments
   end
   root 'posts#index'
-  post '/like/:post_id', to:'likes#like'
-  delete '/like/:post_id', to:'likes#unlike'
+  post '/like/:post_id', to:'likes#like', as: 'like'
+  delete '/like/:post_id', to:'likes#unlike', as: 'unlike'
 end
