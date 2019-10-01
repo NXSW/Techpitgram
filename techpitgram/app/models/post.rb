debce10c1708fee48ffd1f571093722ee8e1417f
49497974m <49497974tkhsm>
2019-10-01 11:43:25 +0900

    Added dependent:: destroy to post model comment

--- techpitgram/app/models/post.rb
+++ techpitgram/app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
-  has_many :comments
+  has_many :comments,dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
 end
