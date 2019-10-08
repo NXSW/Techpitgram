bd7c18e356a3abf50d9e9da9191197f92640ae1a
49497974m <49497974tkhsm>
2019-10-08 16:21:28 +0900

    Set recaptcha's authentication method 'verify_recaptcha?' As a common method
    Described in application_controller.rb

--- techpitgram/app/controllers/application_controller.rb
+++ techpitgram/app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
+require 'net/https'
+require 'uri'
+
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  RECAPTCHA_MINIMUM_SCORE = 0.5
+  RECAPTCHA_ACTION = 'login'
+
+  def verify_recaptcha?(token)
+    secret_key = Rails.application.credencials.recaptcha[:secret_key]
+    uri = URI.parse("https://www.google.com/recaptcha/api/siteverify?secret=#{secret_key}&response=#{token}")
+    r = Net::HTTP.get_response(uri)
+    j = JSON.parse(r.body)
+    j['success'] && j['score'] > RECAPTCHA_MINIMUM_SCORE && j['action'] == RECAPTCHA_ACTION
+  end
 
   protected
 
